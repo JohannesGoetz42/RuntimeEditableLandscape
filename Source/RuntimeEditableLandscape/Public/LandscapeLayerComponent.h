0e48344b5fea1ec52e9e09454bcac45e2a65f136
Johannes Götz <johannesgoetz1991@web.de>
2024-03-06 16:09:25 +0100

    prepare landscape tags

--- Source/RuntimeEditableLandscape/Public/LandscapeLayerComponent.h
+++ Source/RuntimeEditableLandscape/Public/LandscapeLayerComponent.h
@@ -25,6 +25,9 @@ struct FLandscapeLayerData
 	bool bApplyVertexColor = false;
 	UPROPERTY(EditAnywhere)
 	FColor VertexColor = FColor::Red;
+	UPROPERTY(EditAnywhere)
+	/** Custom tags that can be applied to the landscape for querying (i.E. fertility) */
+	TArray<FName> LandscapeTags;
 };
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
@@ -35,6 +38,7 @@ class RUNTIMEEDITABLELANDSCAPE_API ULandscapeLayerComponent : public UActorCompo
 public:
 	void ApplyToLandscape() const;
 	void ApplyLayerData(const FVector2D VertexLocation, float& OutHeightValue, FColor& OutVertexColorValue) const;
+	const TArray<FName>& GetLandscapeTags() const { return LayerData.LandscapeTags; }
 
 	FBox2D GetAffectedArea(bool bIncludeSmoothedArea) const
 	{
@@ -43,7 +47,7 @@ public:
 		GetOwner()->GetActorBounds(false, Origin, Extent);
 		FBox2D Result(FVector2D(Origin.X - Extent.X, Origin.Y - Extent.Y) - FVector2D(LayerData.SmoothingDistance),
 		              FVector2D(Origin.X + Extent.X, Origin.Y + Extent.Y) + FVector2D(LayerData.SmoothingDistance));
-		
+
 		if (bIncludeSmoothedArea)
 		{
 			Result.Min -= FVector2D(LayerData.SmoothingDistance);
